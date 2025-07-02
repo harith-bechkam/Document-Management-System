2cf87e562923f3a5372852fcf78bfa53455ffed9
Harith <harith.kv47@gmail.com>
2025-07-02 10:11:09 +0530

    fix

--- src/pages/admin/UserGroups/ListGroups.js
+++ src/pages/admin/UserGroups/ListGroups.js
@@ -1,143 +1,92 @@
 import React, { useState, useEffect } from "react";
-import { BlockHead, BlockBetween, BlockHeadContent, BlockTitle, BlockDes, Button, Icon, RegularTable } from "../../../components/Component";
+import {
+    BlockHead, BlockBetween, BlockHeadContent, BlockTitle,
+    BlockDes, Button, Icon, RegularTable
+} from "../../../components/Component";
 import * as API from "../../../utils/API";
 import Swal from "sweetalert2";
 import FileManagerLayout from "../../app/file-manager/components/Layout";
 import FilesBody from "../../app/file-manager/components/Body";
-import { Modal, Spinner } from "reactstrap";
+import { Modal } from "reactstrap";
 import CreateGroup from "./AddUserGroup";
 import EditGroup from "./EditUserGroup";
 import UserGroupFilter from "../../../components/Filter/UserGroupFilter";
-import { saveAs } from 'file-saver';
-import * as ExcelJS from 'exceljs';
-import moment from "moment";
-import { updateLoaderFlag } from "../../../redux/folderSlice";
 import { useDispatch } from "react-redux";
+import { updateLoaderFlag } from "../../../redux/folderSlice";
+import { saveAs } from "file-saver";
+import * as ExcelJS from "exceljs";
+import moment from "moment";
+
+const columnMappings = {
+    groupName: "groupName",
+    members: "members.username",
+    isActive: "isActive",
+    created: "createdAt",
+    updated: "updatedAt"
+};
 
 const List = () => {
+    const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
-    const [sm, setSm] = useState(false)
-    const [data, setData] = useState({})
+    const [sm, setSm] = useState(false);
+    const [data, setData] = useState({});
     const [users, setUsers] = useState([]);
-
     const [selectedGroupUsers, setSelectedGroupUsers] = useState([]);
     const [selectedGrp, setSelectedGrp] = useState('');
     const [selectedGrpId, setSelectedGrpId] = useState('');
     const [onSearchText, setOnSearchText] = useState("");
-
     const [createModal, setCreateModal] = useState(false);
     const [editModal, setEditModal] = useState(false);
-
     const [currentPage, setCurrentPage] = useState(1);
     const [itemPerPage, setItemPerPage] = useState(10);
-    const [loader, setLoader] = useState(false);
-    const [sortOrder, setsortOrder] = useState(null)
+    const [sortOrder, setsortOrder] = useState(null);
+    const [rsuiteState, setRsuiteState] = useState(false);
+    const [filter, setFilter] = useState({ users: [], status: [] });
 
     const [columns, setColumns] = useState([
-        {
-            label: "User Group Name",
-            name: "groupName",
-            sort: true,
-            sortOrder: 0,
-            show: true,
-        },
-        {
-            label: "Members",
-            name: "members",
-            sort: true,
-            sortOrder: 0,
-            show: true,
-        },
-        {
-            label: "Status",
-            name: "isActive",
-            sort: true,
-            sortOrder: 0,
-            show: true,
-        },
-        {
-            label: "Created",
-            name: "created",
-            sort: true,
-            sortOrder: 0,
-            show: true,
-        },
-        {
-            label: "Modified",
-            name: "updated",
-            sort: true,
-            sortOrder: 0,
-            show: true,
-        }
-    ])
-    const [rsuiteState, setRsuiteState] = useState(false);
+        { label: "User Group Name", name: "groupName", sort: true, sortOrder: 0, show: true },
+        { label: "Members", name: "members", sort: true, sortOrder: 0, show: true },
+        { label: "Status", name: "isActive", sort: true, sortOrder: 0, show: true },
+        { label: "Created", name: "created", sort: true, sortOrder: 0, show: true },
+        { label: "Modified", name: "updated", sort: true, sortOrder: 0, show: true }
+    ]);
 
     const [extraColumns, setExtraColumns] = useState([
-        {
-            label: "Country",
-            name: "country",
-            checked: false
-        },
-        {
-            label: "State",
-            name: "state",
-            checked: false
-        }
-    ])
-    const [filter, setFilter] = useState({
-        users: [],
-        status: []
-    })
+        { label: "Country", name: "country", checked: false },
+        { label: "State", name: "state", checked: false }
+    ]);
 
+    const Privilege = JSON.parse(localStorage.getItem('privileges'));
 
     useEffect(() => {
-        getAllUsers()
-    }, [])
-
+        getAllUsers();
+    }, []);
 
     useEffect(() => {
-        fetchData()
-    }, [currentPage, itemPerPage, onSearchText, sortOrder, filter])
+        fetchData();
+    }, [currentPage, itemPerPage, onSearchText, sortOrder, filter]);
 
-    const toggleCreateModal = (change = false) => {
-        setCreateModal(!createModal);
-    }
+    const toggleCreateModal = () => setCreateModal(prev => !prev);
+    const toggleEditModal = () => setEditModal(prev => !prev);
 
-    const toggleEditModal = (change = false) => {
-        setEditModal(!editModal);
-    }
+    const fetchData = async (exportData = false) => {
+        const { users, status } = filter;
+        dispatch(updateLoaderFlag({ loader: true, text: 'Fetching UserGroup' }));
 
-    const fetchData = async (
-        exportData = false
-    ) => {
-        // setLoader(true);
+        const userResponse = await API.getUserGroupList(
+            currentPage, itemPerPage, onSearchText,
+            status || [], users, sortOrder, exportData
+        );
 
-        var { users, status } = filter
-        // let statusvalue = status?.['value'] ?? ""
-        // if (statusvalue == "") {
-        //     statusvalue = 'isActive'
-        // }
-        let statusvalue = status || []
-
-
-        dispatch(updateLoaderFlag({ loader: true, text: 'Fetching UserGroup' }))
-        let userResponse = await API.getUserGroupList(currentPage, itemPerPage, onSearchText, statusvalue, users, sortOrder, exportData)
-        if(!exportData){
-            let { ...rem } = userResponse
-            if (userResponse['status']) {
-                setData({
-                    data: userResponse['data'],
-                    ...rem
-                })
-            }
+        if (!exportData && userResponse?.status) {
+            const { status, data, ...rest } = userResponse;
+            setData({ data, ...rest });
         }
-        dispatch(updateLoaderFlag({ loader: false, text: '' }))
-        // setLoader(false);
 
-    }
+        dispatch(updateLoaderFlag({ loader: false, text: '' }));
+    };
 
-    function generateAvatarBase64(char, backgroundColor) {
+    const generateAvatarBase64 = (char, backgroundColor) => {
         const canvas = document.createElement('canvas');
         const ctx = canvas.getContext('2d');
         const size = 24;
@@ -156,202 +105,117 @@ const List = () => {
         ctx.fillText(char, size / 2, size / 2);
 
         return canvas.toDataURL();
-    }
+    };
 
     const getAllUsers = async () => {
-        const respo = await API.getAllUsers();
-        if (respo.status) {
-            let arr = respo.data.map(item => ({
-                label: item.userName, value: item._id, imgUrl: item.imgUrl ? item.imgUrl : generateAvatarBase64(
-                    item.userName[0].toUpperCase(),
-                    '#ccc'
-                )
-            }))
-            setUsers(arr);
+        const res = await API.getAllUsers();
+        if (res.status) {
+            const formatted = res.data.map(u => ({
+                label: u.userName,
+                value: u._id,
+                imgUrl: u.imgUrl || generateAvatarBase64(u.userName[0].toUpperCase(), '#ccc')
+            }));
+            setUsers(formatted);
         }
-    }
+    };
 
     const getSelectedGroup = async (grpId) => {
         await getAllUsers();
-        const respo = await API.getUserGroup(grpId);
-        if (respo.status) {
-            debugger
-            setSelectedGrp(respo.data[0].groupName);
-            setSelectedGrpId(respo.data[0]._id.toString())
-            let arr = respo.data[0].members.map(item => ({ label: item.username, value: item.userId }));
-            setSelectedGroupUsers(arr)
+        const res = await API.getUserGroup(grpId);
+        if (res.status && res.data?.[0]) {
+            const group = res.data[0];
+            setSelectedGrp(group.groupName);
+            setSelectedGrpId(group._id);
+            setSelectedGroupUsers(group.members.map(m => ({ label: m.username, value: m.userId })));
+            toggleEditModal();
         }
-        toggleEditModal();
-    }
+    };
 
-    // const fetchRoles = async () => {
-    //     let roleResponse = await API.getAllRoles()
-    //     if (roleResponse['status']) {
-    //         const rolesOptions = roleResponse['data'].map((role, index) => ({
-    //             value: role.role,
-    //             label: role.role
-    //         }))
-    //         setRoleData(rolesOptions)
-    //     }
-    // }
+    const handlePagination = (pageNumber) => setCurrentPage(pageNumber);
 
-    const Privilege = JSON?.parse(localStorage.getItem('privileges'));
-
-
-
-    const handlePagination = (pageNumber) => setCurrentPage(pageNumber)
-
-    const suspendUser = (id) => {
-        Swal.fire({
+    const suspendUser = async (id) => {
+        const result = await Swal.fire({
             title: "Are you sure you want to delete this usergroup?",
             text: "This action cannot be undone",
             icon: "warning",
             showCancelButton: true,
             confirmButtonText: "Yes, delete it!",
-        }).then(async (result) => {
-            if (result.isConfirmed) {
-                let userResponse = await API.deleteUserGroup(id);
-                if (userResponse['status']) {
-                    Swal.fire("Deleted!", "User Group has been Deleted", "success");
-                    fetchData()
-                }
-                else {
-                    Swal.fire("Error", "Unable to delete User Group", "error");
-                }
-            }
-
         });
-    }
 
-    const sortData = (keycolumnName, sort) => {
-
-        //keycolumnName - dbcol
-        const mappings = {
-            groupName: "groupName",
-            members: "members.username",
-            isActive: "isActive",
-            created: "createdAt",
-            updated: "updatedAt"
+        if (result.isConfirmed) {
+            const res = await API.deleteUserGroup(id);
+            if (res.status) {
+                Swal.fire("Deleted!", "User Group has been Deleted", "success");
+                fetchData();
+            } else {
+                Swal.fire("Error", "Unable to delete User Group", "error");
+            }
         }
+    };
 
-        var dbcol = mappings[keycolumnName]
-
-        setsortOrder({ [dbcol]: sort ? 1 : -1 })
-        setCurrentPage(1)
-
-        // setColumns(prevColumns =>
-        //     prevColumns.map(column =>
-        //         column.name === keycolumnName
-        //             ? { ...column, sortOrder: column.sortOrder === 1 ? -1 : 1 }
-        //             : column
-        //     )
-        // );
-
-        setColumns(prevColumns =>
-            prevColumns.map(column =>
-                column.name === keycolumnName
-                    ? { ...column, sortOrder: column.sortOrder === 1 ? -1 : 1 }
-                    : { ...column, sortOrder: 0 }
-            )
+    const sortData = (key, isAsc) => {
+        const dbKey = columnMappings[key];
+        setsortOrder({ [dbKey]: isAsc ? 1 : -1 });
+        setCurrentPage(1);
+        setColumns(cols =>
+            cols.map(col => ({
+                ...col,
+                sortOrder: col.name === key ? (col.sortOrder === 1 ? -1 : 1) : 0
+            }))
         );
-
-
-        // let defaultData = data['data']
-
-        // if (columnName != 'roleDetails') {
-        //     if (!sort) defaultData.sort((a, b) => a[`${columnName}`].toString().localeCompare(b[`${columnName}`].toString()))
-        //     else defaultData.sort((a, b) => b[`${columnName}`].toString().localeCompare(a[`${columnName}`].toString()))
-        // }
-        // else {
-        //     !sort ? defaultData.sort((a, b) => a[`${columnName}`]['role'].toString().localeCompare(b[`${columnName}`]['role'].toString())) :
-        //         defaultData.sort((a, b) => b[`${columnName}`]['role'].toString().localeCompare(a[`${columnName}`]['role'].toString()))
-        // }
-    }
+    };
 
     const onSearch = (e) => {
+        setOnSearchText(e.target.value);
+        setCurrentPage(1);
+    };
 
-        setOnSearchText(e.target.value)
-        setCurrentPage(1)
-    }
+    const newUseradded = () => {
+        setCurrentPage(1);
+        setsortOrder({ createdAt: -1 });
+    };
 
-    const newUseradded = async () => {
-        setCurrentPage(1)
-        setsortOrder({ createdAt: -1 })
-    }
-
-    async function exportUsers() {
-        console.log(data);
-        // debugger
-        // const reformattedData = data.data.map((group) => ({
-        //     groupName: group.groupName,
-        //     numberOfMembers: group.members.length,
-        //     createdAt: moment(group.createdAt).format('ll'),
-        //     members: group.members.map((member) => member.username).join(", "),
-        // }));
-        // const workbook = new ExcelJS.Workbook();
-        // const usersSheet = workbook.addWorksheet('User-Groups');
-        // usersSheet.columns = [
-        //     { header: 'Group Name', key: 'groupName', width: 15 },
-        //     { header: 'No. of Members', key: 'numberOfMembers', width: 35 },
-        //     { header: 'Group Created On', key: `createdAt`, width: 20 },
-        //     { header: 'Members', key: 'members', width: 20 },
-        // ];
-        // usersSheet.getRow(1).font = { bold: true };
-        // usersSheet.addRows(reformattedData);
-        // workbook.xlsx.writeBuffer().then((buffer) => {
-        //     saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'groups.xlsx');
-        // });
+    const exportUsers = () => {
         fetchData(true);
-    }
-
+    };
 
     return (
         <FileManagerLayout>
-            <FilesBody  >
+            <FilesBody>
                 <BlockHead size="sm">
                     <BlockBetween>
                         <BlockHeadContent>
-                            <BlockTitle tag="h3" page>
-                                User Group List
-                            </BlockTitle>
+                            <BlockTitle tag="h3" page>User Group List</BlockTitle>
                             <BlockDes className="text-soft">
-                                <p>There are {data['totalCount']} groups</p>
+                                <p>There are {data?.totalCount || 0} groups</p>
                             </BlockDes>
                         </BlockHeadContent>
                         <BlockHeadContent>
                             <div className="toggle-wrap nk-block-tools-toggle">
-                                <Button
-                                    className={`btn-icon btn-trigger toggle-expand me-n1 ${sm ? "active" : ""}`}
-                                    onClick={() => setSm(!sm)}
-                                >
-                                    <Icon name="menu-alt-r"></Icon>
+                                <Button className={`btn-icon btn-trigger toggle-expand me-n1 ${sm ? "active" : ""}`} onClick={() => setSm(!sm)}>
+                                    <Icon name="menu-alt-r" />
                                 </Button>
                                 <div className="toggle-expand-content" style={{ display: sm ? "block" : "none" }}>
                                     <ul className="nk-block-tools g-3">
                                         <li>
-                                            <Button onClick={() => exportUsers()} color="light" outline className="btn-white">
-                                                <Icon name="download-cloud"></Icon>
-                                                <span>Export</span>
+                                            <Button onClick={exportUsers} color="light" outline className="btn-white">
+                                                <Icon name="download-cloud" /><span>Export</span>
                                             </Button>
-                                        </li> 
-                                        {Privilege['userGroup']['adduserGroup'] &&
+                                        </li>
+                                        {Privilege?.userGroup?.adduserGroup && (
                                             <li className="nk-block-tools-opt">
-                                                <Button color="primary"
-                                                    onClick={() =>
-                                                        toggleCreateModal()
-                                                    }
-                                                >
-                                                    <Icon name="plus"></Icon>
-                                                    <span>Add Group</span>
+                                                <Button color="primary" onClick={toggleCreateModal}>
+                                                    <Icon name="plus" /><span>Add Group</span>
                                                 </Button>
                                             </li>
-                                        }
+                                        )}
                                     </ul>
                                 </div>
                             </div>
                         </BlockHeadContent>
                     </BlockBetween>
                 </BlockHead>
+
                 <RegularTable
                     columns={columns}
                     extraColumns={extraColumns}
@@ -364,15 +228,11 @@ const List = () => {
                     handlePagination={handlePagination}
                     onSearchText={onSearchText}
                     onSearchChange={onSearch}
-
                     privilege={{
-                        editAccess: Privilege['userGroup']['updateuserGroup'],
-                        deleteAccess: Privilege['userGroup']['deleteuserGroup']
+                        editAccess: Privilege?.userGroup?.updateuserGroup,
+                        deleteAccess: Privilege?.userGroup?.deleteuserGroup
                     }}
-                    // onEditClick={(id) => navigate(`/users/edit/${id}`)}
-                    onEditClick={(id) => {
-                        getSelectedGroup(id)
-                    }}
+                    onEditClick={getSelectedGroup}
                     suspendUser={suspendUser}
                     sortData={sortData}
                     filterComp={
@@ -388,9 +248,11 @@ const List = () => {
                         />
                     }
                 />
+
                 <Modal isOpen={createModal} size="md" toggle={toggleCreateModal}>
                     <CreateGroup toggle={toggleCreateModal} users={users} newUseradded={newUseradded} />
                 </Modal>
+
                 <Modal isOpen={editModal} size="md" toggle={toggleEditModal}>
                     <EditGroup
                         toggle={toggleEditModal}
@@ -403,8 +265,7 @@ const List = () => {
                 </Modal>
             </FilesBody>
         </FileManagerLayout>
-
-    )
-}
+    );
+};
 
 export default List;
